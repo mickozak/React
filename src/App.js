b151c728184abfbfd784fc722b347acb23713ebb
MKozak <kozak_m@outlook.com>
2018-04-29 11:17:06 +0200

    Counter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WelcomeFunctionComponent from './components/WelcomeFunctionComponent'
 import WelcomeClassComponent from './components/WelcomeClassComponent'
 import User from './components/User'
 import Clock from './components/Clock'
+import Counter from './components/Counter'
 
 
 
@@ -15,9 +16,14 @@ const App = (props) => (
         <WelcomeFunctionComponent name='Display the prop! - 1'/>
         <WelcomeFunctionComponent name='Display the prop! - 2'/>
         <WelcomeFunctionComponent name='Display the prop! - 3'/>
+        <hr/>
         <WelcomeClassComponent name='Display the prop! - 1'/>
+        <hr/>
         <User/>
+        <hr/>
         <Clock/>
+        <hr/>
+        <Counter/>
     </div>
 )
 
---
+++ src/components/Counter.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const styles = {
+    btn: {
+        margin: '10px'
+    }
+}
+
+class Counter extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+        this.minusBtnClick = () => this.setState({count: this.state.count-1})
+        this.plusBtnClick = () => this.setState({count: this.state.count+1})
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Counter - State - Material-UI</h1>
+                <h2>{this.state.count}</h2>
+                <RaisedButton
+                label={'-'}
+                primary={true}
+                onClick={this.minusBtnClick}
+                style={styles.btn}
+                />
+                <RaisedButton
+                label={'+'}
+                primary={false}
+                onClick={this.plusBtnClick}
+                style={styles.btn}
+                />
+            </div>
+        )
+    }
+}
+
+export default Counter
