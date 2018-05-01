8940ae6b6f414caba3825e1cd72cd85c0987b5da
MKozak <kozak_m@outlook.com>
2018-05-01 11:41:20 +0200

    CompleteAppProductDataTable

--- src/productdatatable/SearchBar.js
+++ src/productdatatable/SearchBar.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleInStockChange = this.handleInStockChange.bind(this)
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+    }
+
+    handleFilterTextChange(e){
+        this.props.onFilterTextChange(e.target.value)
+    }
+
+    handleInStockChange(e){
+        this.props.onInStockChange(e.target.checked)
+    }
+
     render (){
+
         return(
             <form>
-                <input type='text' placeholder='Search...'/>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    value={this.props.filterText}
+                    onChange={this.handleFilterTextChange}
+                />
                 <p>
-                    <input type='checkbox'/>
+                    <input type='checkbox'
+                    checked={this.props.inStockOnly}
+                    onChange={this.handleInStockChange}/>
                     {' '}
                     Only show products in stock.
                 </p>
