6bc5a063bef6314fc421654c703555f2d0d9e2f4
MKozak <kozak_m@outlook.com>
2018-05-01 10:36:50 +0200

    StaticVersionAppProductDataTable

---
+++ src/productdatatable/FilterableProductTable.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import SearchBar from './SearchBar'
+import ProductTable from './ProductTable'
+
+class FilterableProductTable extends React.Component{
+    render(){
+        return(
+            <div>
+                <SearchBar/>
+                <ProductTable products={this.props.products}/>
+            </div>
+        )
+    }
+
+}
+ export default FilterableProductTable
